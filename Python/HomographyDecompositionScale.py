c49e4610776fa51597ef5277d576b64deb63bdaf
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-05-25 10:54:56 -0500

    Fourth example

--- Python/HomographyDecompositionScale.py
+++ Python/HomographyDecompositionScale.py
@@ -4,7 +4,7 @@
 	@email: patricia.tavares@cimat.mx
 	version: 3.0
 	This code uses homography decomposition to achieve a formation using
-	scale information as in paper Vision-based distributed formation 
+	scale information as in paper Vision-based distributed formation
 	without an external positioning system.
 """
 """
@@ -17,7 +17,7 @@ from Functions.Geometric import move_wf, rigidity_function, RMF
 #######################################ALWAYS NEEDED
 import numpy as np
 from math import pi
-import shutil, os
+import shutil, os, argparse, logging
 from random import  seed
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -30,9 +30,18 @@ from Functions.Error import get_error, transform_to_frame, distances
 """
 """
 	function: main
-	description: example of how to use the functions and the consensus 
+	description: example of how to use the functions and the consensus
 	algorithm for control with relative positions and rotations.
 """
+# Parser arguments
+parser = argparse.ArgumentParser(description='example of how to use the functions and the consensus algorithm for control with relative positions and rotations')
+parser.add_argument('--log_level',type=int, default=20,help='Log level (default: 20)')
+parser.add_argument('--log_file',default='',help='Log file (default: standard output)')
+args = parser.parse_args()
+
+# Loggin format
+logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
+
 #================================================================points in scene
 xx = np.array([0.72,1.25,1.25,0.75]) #place here the X
 yy = np.array([1.25,1.25,0.75,0.75]) #place here the y
@@ -45,20 +54,20 @@ seed(40200) #init seed for comparision purposes
 n_cameras = 5
 init_cameras, init_poses = verified_random_restricted(n_cameras,0.5,[0.,2.,0.5,1.5,1.,3.,0.,0.,0.,0.,-pi/2.,pi/2.])
 desired_cameras, desired_poses = circle_formation(n_cameras,1.,[1.,1.])
-init_cameras, init_poses = order_cameras(n_cameras, init_cameras,init_poses, desired_poses)	
+init_cameras, init_poses = order_cameras(n_cameras, init_cameras,init_poses, desired_poses)
 copy = copy_cameras(n_cameras,init_poses)
 
-#===================================getting Laplacian and desired relative poses 
+#===================================getting Laplacian and desired relative poses
 L = get_L_radius(n_cameras,init_poses,1.5)
 A = get_A(n_cameras,L)
-p_aster,R_aster = get_relative(n_cameras,desired_cameras,L) 
+p_aster,R_aster = get_relative(n_cameras,desired_cameras,L)
 #transform translations to cameras 1 frame
-p_n_a = transform_to_frame(desired_cameras[0].R,p_aster,desired_cameras)	
+p_n_a = transform_to_frame(desired_cameras[0].R,p_aster,desired_cameras)
 
 #==================================================================define params
 # Define the gain
 lambdav         = 2.
-lambdaw         = 6.	
+lambdaw         = 6.
 # Timing parameters
 dt = 0.01   # Time Delta, seconds.
 ite = 0 #iterations
@@ -67,7 +76,7 @@ max_ite = 10000 #max iterations
 threshold = 1e-4 #threshold for errors
 e_t = 10000 #actual error for translation
 e_psi = 10000 #actual error for rotation
-p = {} #for filter 
+p = {} #for filter
 #=============================================================plotting variables
 t_arr = [] #time array
 err_t = [] #average error in translation
@@ -79,7 +88,7 @@ v = [] #velocity
 x = [] #pos x for every camera
 y = [] #pos y for every camera
 z = [] #pos z for every camera
-for i in range(n_cameras):	
+for i in range(n_cameras):
 	x.append([])
 	y.append([])
 	z.append([])
@@ -90,44 +99,44 @@ rf_aster = rigidity_function(p_aster)
 J = RMF(rf_aster,n_cameras,L,desired_cameras,p_aster,R_aster)
 rank = np.linalg.matrix_rank(J)
 u,s,vt = np.linalg.svd(J)
-print 'Desired Formation'
-print 'Jacobian dimension ', J.shape
-print 'Jacobian rank ',rank
-print 'Sixth eigenvalue of J.TJ', s[s.shape[0]-6]
+logging.info('Desired Formation')
+logging.info('Jacobian dimension {}'.format(J.shape))
+logging.info('Jacobian rank  {}'.format(rank))
+logging.info('Sixth eigenvalue of J.TJ {}'.format(s[s.shape[0]-6]))
 
-#=================================================================init algorithm 
+#=================================================================init algorithm
 while (e_t > threshold or e_psi > threshold) and ite < max_ite and rank >= 4*n_cameras-5:
 	#make a step in scale consensus
 	g = A.dot(g)
-	#Compute velocities 
+	#Compute velocities
 	p, R, vi, wi = HDC(n_cameras,init_cameras,w_points,n_points,R_aster,p_aster,lambdaw,lambdav,L,p,g)
 	#compute error
 	p_n = transform_to_frame(init_cameras[0].R,p,init_cameras)#transform to camera 1 frame
-	
+
 	e_t, e_psi, e_s = get_error(p_n,p_n_a,R,R_aster,init_cameras,desired_cameras, None, None)
 	err_t.append(e_t)
-	err_psi.append(e_psi)	
+	err_psi.append(e_psi)
 
 	#getting back to world frame with those velocities
-	init_poses = move_wf(n_cameras,init_cameras,vi,wi,dt)			
-	#assing the new pose		
+	init_poses = move_wf(n_cameras,init_cameras,vi,wi,dt)
+	#assing the new pose
 	for i in range(n_cameras):
 		x[i].append(init_poses[i][0])
 		y[i].append(init_poses[i][1])
 		z[i].append(init_poses[i][2])
-		init_cameras[i].set_position(init_poses[i][0],init_poses[i][1],init_poses[i][2],init_poses[i][3],init_poses[i][4],init_poses[i][5])	
-	#save info for plots		
+		init_cameras[i].set_position(init_poses[i][0],init_poses[i][1],init_poses[i][2],init_poses[i][3],init_poses[i][4],init_poses[i][5])
+	#save info for plots
 	v.append(sum(vi)/n_cameras)
-	w.append(sum(np.abs(wi))/n_cameras)		
-	t_arr.append(ite*dt)	
+	w.append(sum(np.abs(wi))/n_cameras)
+	t_arr.append(ite*dt)
 	gamma.append(g[0])
-				
-	#Next		
+
+	#Next
 	ite+=1
-	print ite,'Translation error: ',e_t, 'Rotation error: ', e_psi
-	
-print "*********************\nLaplacian is\n", L
-#=======================================================================plotting 
+	logging.info('{} Translation error: {:.4f} Rotation error: {:.4f} '.format(ite,e_t ,e_psi))
+
+logging.info("*********************\nLaplacian is {}\n".format(L))
+#=======================================================================plotting
 #converting to numpy
 v = np.array(v)
 w = np.array(w)
@@ -143,17 +152,17 @@ if 'graphs' in dirs:
 os.mkdir('graphs')
 
 #plotting
-rows = 3
+rows = 4
 cols = 2
-ax,fig = configure_plot(rows,cols,15,20,'Position-based control using Homography Decomposition with scale estimation')
+fig = configure_plot(rows,cols,15,20,'Position-based control using Homography Decomposition with scale estimation')
 bounds = [min(-1.0,np.min(x)-0.2,np.min(y)-0.2,np.min(z)-0.2),max(2.5,np.max(x)+0.2,np.max(y)+0.2,np.max(z)+0.2)]
-ax,fig = plot_all_cameras(ax,fig,n_cameras,init_cameras,desired_cameras,copy,init_poses,desired_poses,x,y,z,0.5,0.09,bounds)
+ax,fig = plot_all_cameras(fig,[rows//2,cols,1],n_cameras,init_cameras,desired_cameras,copy,init_poses,desired_poses,x,y,z,0.5,0.09,bounds)
 ax.plot(xx,yy,zz,'o', alpha=0.2) #plot points
-ax,fig = plot_quarter(ax,fig,[rows,cols,2],t_arr,gamma,'Scale consensus',ite,None,['Time (s)','Scale'],4)
-ax,fig = plot_quarter(ax,fig,[rows,cols,3],t_arr,v,['$v_x$','$v_y$','$v_z$'],ite,None,['Time (s)','Average linear velocity (m/s)'])
-ax,fig = plot_quarter(ax,fig,[rows,cols,4],t_arr,w,['$\omega_x$','$\omega_y$','$\omega_z$'],ite,None,['Time (s)','Average absolute angular velocity (rad/s)'])
-ax,fig = plot_quarter(ax,fig,[rows,cols,5],t_arr,err_t,'Evaluation error in translation ($e_t$)',ite,None,['Time (s)','Evaluation error in translation (m)'])
-ax,fig = plot_quarter(ax,fig,[rows,cols,6],t_arr,err_psi,'Evaluation error in rotation ($e_\psi$)',ite,None,['Time (s)','Evaluation error in rotation (rad)'])
+ax,fig = plot_quarter(fig,[rows,cols,2],t_arr,gamma,'Scale consensus',ite,None,['Time (s)','Scale'],4)
+ax,fig = plot_quarter(fig,[rows,cols,5],t_arr,v,['$v_x$','$v_y$','$v_z$'],ite,None,['Time (s)','Average linear velocity (m/s)'])
+ax,fig = plot_quarter(fig,[rows,cols,6],t_arr,w,['$\omega_x$','$\omega_y$','$\omega_z$'],ite,None,['Time (s)','Average absolute angular velocity (rad/s)'])
+ax,fig = plot_quarter(fig,[rows,cols,7],t_arr,err_t,'Evaluation error in translation ($e_t$)',ite,None,['Time (s)','Evaluation error in translation (m)'])
+ax,fig = plot_quarter(fig,[rows,cols,8],t_arr,err_psi,'Evaluation error in rotation ($e_\psi$)',ite,None,['Time (s)','Evaluation error in rotation (rad)'])
 
 #save and show
 plt.savefig('graphs/complete.pdf',bbox_inches='tight')

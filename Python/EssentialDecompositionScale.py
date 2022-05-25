32e7a852fd12bb65ce80736afd2d6d2d354a556e
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-05-24 20:53:00 -0500

    Import

---
+++ Python/EssentialDecompositionScale.py
@@ -0,0 +1,166 @@
+"""
+	Saturday September 22, 23:41:10 2018
+	@author: robotics lab (Patricia Tavares)
+	@email: patricia.tavares@cimat.mx
+	version: 3.0
+	This code uses essential decomposition to achieve a formation 
+	estimating scale.
+"""
+"""
+	Import section: from python and robotics.
+"""
+from Functions.Formation import line_formation, circle_formation, get_L_radius, get_relative, get_A
+from Functions.Initialize import verified_random_restricted, copy_cameras, order_cameras,find_nearest
+from Functions.Control import EDC
+from Functions.Geometric import move_wf, RMF, rigidity_function
+#######################################ALWAYS NEEDED
+import numpy as np
+from math import pi
+import shutil, os
+from random import  seed
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from Functions.Plot import *
+from Functions.PlanarCamera import PlanarCamera
+from Functions.Error import get_error, transform_to_frame, distances
+
+"""
+	***************************************************************************************************Example
+"""
+"""
+	function: main
+	description: example of how to use the functions and the consensus 
+	algorithm for control with relative positions and rotations using
+	scale estimation.
+"""
+#================================================================points in scene
+xx       = np.loadtxt('cloud/x.data')
+yy       = np.loadtxt('cloud/y.data')
+zz       = np.loadtxt('cloud/z.data')
+n_points = len(xx)
+w_points = np.vstack([xx, yy, zz])
+
+#===================================================================init cameras
+#seed(40200) #init seed for comparision purposes
+n_cameras = 5
+init_cameras, init_poses = verified_random_restricted(n_cameras,0.5,[0.,2.,0.5,1.5,1.,3.,0.,0.,0.,0.,-pi/2.,pi/2.])
+desired_cameras, desired_poses = circle_formation(n_cameras,1.,[1.,1.])
+init_cameras, init_poses = order_cameras(n_cameras, init_cameras,init_poses, desired_poses)	
+copy = copy_cameras(n_cameras,init_poses)
+#choosing the point closest to the center
+cloud_center = np.array([1.0,1.0,-0.5]) #center of the cloud
+spindex = find_nearest(n_points,w_points,cloud_center) #index of the point	
+
+#===================================getting Laplacian and desired relative poses 
+L = get_L_radius(n_cameras,init_poses,1.5)
+A = get_A(n_cameras,L)
+p_aster,R_aster = get_relative(n_cameras,desired_cameras,L) 
+#transform translations to cameras 1 frame
+p_n_a = transform_to_frame(desired_cameras[0].R,p_aster,desired_cameras)	
+#obtaining distances between drones in desired formation
+dist_aster = distances(n_cameras, desired_cameras)
+
+#==================================================================define params
+# Define the gain
+lambdav         = 2.
+lambdaw         = 6.	
+# Timing parameters
+dt = 0.01   # Time Delta, seconds.
+ite = 0 #iterations
+max_ite = 10000 #max iterations
+# Error parameters
+threshold = 1e-4 #threshold for errors
+e_t = 10000 #actual error for translation
+e_psi = 10000 #actual error for rotation
+p = {} #relative positions
+R = {} #relative rotations
+#=============================================================plotting variables
+t_arr = [] #time array
+err_t = [] #average error in translation
+err_psi = [] #average error in rotation
+gamma = [] #for scale information
+g = [] #for scale consensus
+w = [] #angular velocity
+v = [] #velocity
+x = [] #pos x for every camera
+y = [] #pos y for every camera
+z = [] #pos z for every camera
+for i in range(n_cameras):	
+	x.append([])
+	y.append([])
+	z.append([])	
+	g.append(init_poses[i][2])
+
+#Verify the formation
+rf_aster = rigidity_function(p_aster)
+J = RMF(rf_aster,n_cameras,L,desired_cameras,p_aster,R_aster)
+rank = np.linalg.matrix_rank(J)
+u,s,vt = np.linalg.svd(J)
+print 'Desired Formation'
+print 'Jacobian dimension ', J.shape
+print 'Jacobian rank ',rank
+print 'Sixth eigenvalue of J.TJ', s[s.shape[0]-6]
+
+#=================================================================init algorithm 
+while (e_t > threshold or e_psi > threshold) and ite < max_ite and rank >= 4*n_cameras-5:
+	#compute scale consensus
+	g = A.dot(g)
+	#Compute velocities 
+	p, R, vi, wi = EDC(n_cameras,init_cameras,w_points,n_points,R_aster,p_aster,lambdaw,lambdav,L,p,R,g,spindex)
+	#compute error
+	p_n = transform_to_frame(init_cameras[0].R,p,init_cameras)#transform to camera 1 frame
+	dist = distances(n_cameras,init_cameras)#computing real distances for simulation purposes	
+	e_t, e_psi, e_s = get_error(p_n,p_n_a,R,R_aster,init_cameras,desired_cameras, dist, dist_aster)
+	err_t.append(e_t)
+	err_psi.append(e_psi)	
+	gamma.append(g[0])
+
+	#getting back to world frame with those velocities
+	init_poses = move_wf(n_cameras,init_cameras,vi,wi,dt)			
+	#assing the new pose		
+	for i in range(n_cameras):
+		x[i].append(init_poses[i][0])
+		y[i].append(init_poses[i][1])
+		z[i].append(init_poses[i][2])
+		init_cameras[i].set_position(init_poses[i][0],init_poses[i][1],init_poses[i][2],init_poses[i][3],init_poses[i][4],init_poses[i][5])	
+	#save info for plots		
+	v.append(sum(vi)/n_cameras)
+	w.append(sum(np.abs(wi))/n_cameras)		
+	t_arr.append(ite*dt)
+
+	#Next		
+	ite+=1
+	print ite,'Translation error: ',e_t, 'Rotation error: ', e_psi
+	
+print "*********************\nLaplacian is\n", L
+#=======================================================================plotting 
+#converting to numpy
+v = np.array(v)
+w = np.array(w)
+t_arr = np.array(t_arr)
+err_t = np.array(err_t)
+err_psi = np.array(err_psi)
+gamma = np.array(gamma)
+
+#clearing
+dirs = os.listdir('.')
+if 'graphs' in dirs:
+	shutil.rmtree("graphs")
+os.mkdir('graphs')
+
+#plotting
+rows = 3
+cols = 2
+ax,fig = configure_plot(rows,cols,15,20,'Position-based control using Essential Decomposition with scale estimation')
+bounds = [min(-1.0,np.min(x)-0.2,np.min(y)-0.2,np.min(z)-0.2),max(2.5,np.max(x)+0.2,np.max(y)+0.2,np.max(z)+0.2)]
+ax,fig = plot_all_cameras(ax,fig,n_cameras,init_cameras,desired_cameras,copy,init_poses,desired_poses,x,y,z,0.5,0.09,bounds)
+ax.plot(xx,yy,zz,'o', alpha=0.2) #plot points
+ax,fig = plot_quarter(ax,fig,[rows,cols,2],t_arr,gamma,'Scale consensus',ite,None,['Time (s)','Scale'],4)
+ax,fig = plot_quarter(ax,fig,[rows,cols,3],t_arr,v,['$v_x$','$v_y$','$v_z$'],ite,None,['Time (s)','Average linear velocity (m/s)'])
+ax,fig = plot_quarter(ax,fig,[rows,cols,4],t_arr,w,['$\omega_x$','$\omega_y$','$\omega_z$'],ite,None,['Time (s)','Average absolute angular velocity (rad/s)'])
+ax,fig = plot_quarter(ax,fig,[rows,cols,5],t_arr,err_t,'Evaluation error in translation ($e_t$)',ite,None,['Time (s)','Evaluation error in translation (m)'])
+ax,fig = plot_quarter(ax,fig,[rows,cols,6],t_arr,err_psi,'Evaluation error in rotation ($e_\psi$)',ite,None,['Time (s)','Evaluation error in rotation (rad)'])
+
+#save and show
+plt.savefig('graphs/complete.pdf',bbox_inches='tight')
+plt.show()
---
+++ Python/HomographyDecompositionNoScale.py
@@ -0,0 +1,155 @@
+"""
+	Saturday September 22, 22:25:12 2018
+	@author: robotics lab (Patricia Tavares)
+	@email: patricia.tavares@cimat.mx
+	version: 3.0
+	This code uses homography decomposition to achieve a formation.
+"""
+"""
+	Import section: from python and robotics.
+"""
+from Functions.Formation import line_formation, circle_formation, get_L_radius, get_relative
+from Functions.Initialize import verified_random_restricted, copy_cameras, order_cameras
+from Functions.Control import HDC
+from Functions.Geometric import move_wf, rigidity_function, RMF
+#######################################ALWAYS NEEDED
+import numpy as np
+from math import pi
+import shutil, os
+from random import  seed
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from Functions.Plot import *
+from Functions.PlanarCamera import PlanarCamera
+from Functions.Error import get_error, transform_to_frame, distances
+
+"""
+	***************************************************************************************************Example
+"""
+"""
+	function: main
+	description: example of how to use the functions and the consensus 
+	algorithm for control with relative positions and rotations.
+"""
+#================================================================points in scene
+xx = np.array([0.72,1.25,1.25,0.75]) #place here the X
+yy = np.array([1.25,1.25,0.75,0.75]) #place here the y
+zz = np.array([-1,-1,-1,-1]) #place here the Z
+w_points = np.vstack([xx, yy, zz])
+n_points = 4
+
+#===================================================================init cameras
+#seed(40200) #init seed for comparision purposes
+n_cameras = 5
+init_cameras, init_poses = verified_random_restricted(n_cameras,0.5,[0.,2.,0.5,1.5,1.,3.,0.,0.,0.,0.,-pi/2.,pi/2.])
+desired_cameras, desired_poses = circle_formation(n_cameras,1.,[1.,1.])
+init_cameras, init_poses = order_cameras(n_cameras, init_cameras,init_poses, desired_poses)	
+copy = copy_cameras(n_cameras,init_poses)
+
+#===================================getting Laplacian and desired relative poses 
+L = get_L_radius(n_cameras,init_poses,1.5)
+p_aster,R_aster = get_relative(n_cameras,desired_cameras,L) 
+#transform translations to cameras 1 frame
+p_n_a = transform_to_frame(desired_cameras[0].R,p_aster,desired_cameras)	
+#obtaining distances between drones in desired formation
+dist_aster = distances(n_cameras, desired_cameras)
+
+#==================================================================define params
+# Define the gain
+lambdav         = 2.
+lambdaw         = 6.	
+# Timing parameters
+dt = 0.01   # Time Delta, seconds.
+ite = 0 #iterations
+max_ite = 10000 #max iterations
+# Error parameters
+threshold = 1e-4 #threshold for errors
+e_t = 10000 #actual error for translation
+e_psi = 10000 #actual error for rotation
+p = {} #for relative positions filter
+#=============================================================plotting variables
+t_arr = [] #time array
+err_t = [] #average error in translation
+err_psi = [] #average error in rotation
+err_s = [] #error for scale
+w = [] #angular velocity
+v = [] #velocity
+x = [] #pos x for every camera
+y = [] #pos y for every camera
+z = [] #pos z for every camera
+for i in range(n_cameras):	
+	x.append([])
+	y.append([])
+	z.append([])	
+
+#Verify the formation, it has to be rigid
+rf_aster = rigidity_function(p_aster)
+J = RMF(rf_aster,n_cameras,L,desired_cameras,p_aster,R_aster)
+rank = np.linalg.matrix_rank(J)
+u,s,vt = np.linalg.svd(J)
+print 'Desired Formation'
+print 'Jacobian dimension ', J.shape
+print 'Jacobian rank ',rank
+print 'Sixth eigenvalue of J.TJ', s[s.shape[0]-6]
+
+#=================================================================init algorithm 
+while (e_t > threshold or e_psi > threshold) and ite < max_ite and rank >= 4*n_cameras-5:
+	#Compute velocities 
+	p, R, vi, wi = HDC(n_cameras,init_cameras,w_points,n_points,R_aster,p_aster,lambdaw,lambdav,L,p)
+	#compute error
+	p_n = transform_to_frame(init_cameras[0].R,p,init_cameras)#transform to camera 1 frame
+	dist = distances(n_cameras,init_cameras)#computing real distances for simulation purposes	
+	e_t, e_psi, e_s = get_error(p_n,p_n_a,R,R_aster,init_cameras,desired_cameras, dist, dist_aster)
+	err_t.append(e_t)
+	err_psi.append(e_psi)	
+	err_s.append(e_s)
+
+	#getting back to world frame with those velocities
+	init_poses = move_wf(n_cameras,init_cameras,vi,wi,dt)			
+	#assing the new pose		
+	for i in range(n_cameras):
+		x[i].append(init_poses[i][0])
+		y[i].append(init_poses[i][1])
+		z[i].append(init_poses[i][2])
+		init_cameras[i].set_position(init_poses[i][0],init_poses[i][1],init_poses[i][2],init_poses[i][3],init_poses[i][4],init_poses[i][5])	 
+
+	#save info for plots		
+	v.append(sum(vi)/n_cameras)
+	w.append(sum(np.abs(wi))/n_cameras)		
+	t_arr.append(ite*dt)					
+	#Next		
+	ite+=1
+	print ite,'Translation error: ',e_t, 'Rotation error: ', e_psi
+	
+print "*********************\nLaplacian is\n", L
+#=======================================================================plotting 
+#converting to numpy
+v = np.array(v)
+w = np.array(w)
+t_arr = np.array(t_arr)
+err_t = np.array(err_t)
+err_psi = np.array(err_psi)
+err_s = np.array(err_s)
+
+#clearing
+dirs = os.listdir('.')
+if 'graphs' in dirs:
+	shutil.rmtree("graphs")
+os.mkdir('graphs')
+
+#plotting
+rows = 3
+cols = 2
+ax,fig = configure_plot(rows,cols,15,20,'Position-based control using Homography Decomposition without scale estimation')
+bounds = [min(-1.0,np.min(x)-0.2,np.min(y)-0.2,np.min(z)-0.2),max(2.5,np.max(x)+0.2,np.max(y)+0.2,np.max(z)+0.2)]
+ax,fig = plot_all_cameras(ax,fig,n_cameras,init_cameras,desired_cameras,copy,init_poses,desired_poses,x,y,z,0.5,0.09,bounds)
+ax.plot(xx,yy,zz,'o', alpha=0.2) #plot points
+ax,fig = plot_quarter(ax,fig,[rows,cols,2],t_arr,err_s,'Actual formation average scale',ite,None,['Time (s)','Actual formation average scale'],4)
+ax,fig = plot_quarter(ax,fig,[rows,cols,3],t_arr,v,['$v_x$','$v_y$','$v_z$'],ite,None,['Time (s)','Average linear velocity (m/s)'])
+ax,fig = plot_quarter(ax,fig,[rows,cols,4],t_arr,w,['$\omega_x$','$\omega_y$','$\omega_z$'],ite,None,['Time (s)','Average absolute angular velocity (rad/s)'])
+ax,fig = plot_quarter(ax,fig,[rows,cols,5],t_arr,err_t,'Evaluation error in translation ($e_t$)',ite,None,['Time (s)','Evaluation error in translation (m)'])
+ax,fig = plot_quarter(ax,fig,[rows,cols,6],t_arr,err_psi,'Evaluation error in rotation ($e_\psi$)',ite,None,['Time (s)','Evaluation error in rotation (rad)'])
+
+#save and show
+plt.savefig('graphs/complete.pdf',bbox_inches='tight')
+plt.show()

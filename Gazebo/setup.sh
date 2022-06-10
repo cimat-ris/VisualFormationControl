bd6effe49fb1c949d1ae4fe7cdb03cd37c737b2c
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-06-10 16:23:17 -0500

    Copy one more file through setup

--- Gazebo/setup.sh
+++ Gazebo/setup.sh
@@ -2,5 +2,7 @@
 WORKSPACE=~/catkin_ws/
 cp -r Files/mygroundplane/ ~/.gazebo/models/
 cp Files/ground.world ${WORKSPACE}/src/rotors_simulator/rotors_description/urdf/
+cp Files/hummingbird.xacro ${WORKSPACE}/src/rotors_simulator/rotors_description/urdf/hummingbird.xacro
 cp -r Packages/Placing ${WORKSPACE}/src/
+cp -r Packages/homography ${WORKSPACE}/src/
 source ${WORKSPACE}/devel/setup.bash

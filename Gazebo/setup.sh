feeca9ffb0a24438102a5d6a14dd48b7162992b8
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-06-10 17:38:48 -0500

    Error in one of the copied files

--- Gazebo/setup.sh
+++ Gazebo/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 WORKSPACE=~/catkin_ws/
 cp -r Files/mygroundplane/ ~/.gazebo/models/
-cp Files/ground.world ${WORKSPACE}/src/rotors_simulator/rotors_description/urdf/
+cp Files/ground.world ${WORKSPACE}/src/rotors_simulator/rotors_gazebo/worlds/
 cp Files/hummingbird.xacro ${WORKSPACE}/src/rotors_simulator/rotors_description/urdf/hummingbird.xacro
 cp -r Packages/Placing ${WORKSPACE}/src/
 cp -r Packages/homography ${WORKSPACE}/src/

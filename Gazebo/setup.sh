86ad53138fbb175a9c02649e1fa088e5d16bfcbf
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-07-22 15:45:19 -0500

    Chages package name

--- Gazebo/setup.sh
+++ Gazebo/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-WORKSPACE=~/catkin_ws
+WORKSPACE=~/catkin_ws_vc
 cp -r Files/mygroundplane/ ~/.gazebo/models/
 cp Files/ground.world ${WORKSPACE}/src/rotors_simulator/rotors_gazebo/worlds/
 cp Files/hummingbird.xacro ${WORKSPACE}/src/rotors_simulator/rotors_description/urdf/hummingbird.xacro
 cp -r Packages/Placing ${WORKSPACE}/src/
-cp -r Packages/homography ${WORKSPACE}/src/
-echo "target_compile_definitions(homography PUBLIC WORKSPACE=\"${WORKSPACE}\")" >> ${WORKSPACE}/src/homography/CMakeLists.txt
+cp -r Packages/vc_controller ${WORKSPACE}/src/
+echo "target_compile_definitions(vc_controller PUBLIC WORKSPACE=\"${WORKSPACE}\")" >> ${WORKSPACE}/src/vc_controller/CMakeLists.txt
 source ${WORKSPACE}/devel/setup.bash

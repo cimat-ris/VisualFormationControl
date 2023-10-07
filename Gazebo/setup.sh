b917e6aaaa49fb701912d6793a50346bb6123810
bloodfield2 <edgar.cimat@cimat.mx>
2023-10-07 00:04:26 -0600

    Gazebo: actualización de formation IBVS

--- Gazebo/setup.sh
+++ Gazebo/setup.sh
@@ -11,27 +11,37 @@ then
     echo -e "\tIBVS \t vc_controller"
     exit
 else
-    WORKSPACE="$1"
+    WORKSPACE=${1%/}
 fi
 
 
-if [ "$2" == "Files" ] || [ "$2" == "All" ]
-then
-cp Files/multiple_hummingbird_nodes_*.launch ${WORKSPACE}/src/rotors_simulator/rotors_gazebo/launch/
-cp -r Files/mygroundplane/ ~/.gazebo/models/
-cp Files/ground.world ${WORKSPACE}/src/rotors_simulator/rotors_gazebo/worlds/
-cp Files/hummingbird.xacro ${WORKSPACE}/src/rotors_simulator/rotors_description/urdf/hummingbird.xacro
-fi
+
 
 if [ "$2" == "IBFC" ] || [ "$2" == "All" ]
 then
 cp -r Packages/image_based_formation_control ${WORKSPACE}/src/
+echo "# Directories, select desired input" >> ${WORKSPACE}/src/image_based_formation_control/config/params.yaml
+echo "input_dir: \"${WORKSPACE}/src/image_based_formation_control/config/\"" >> ${WORKSPACE}/src/image_based_formation_control/config/params.yaml
+echo "output_dir: \"${WORKSPACE}/image_based_formation_control/output/\"" >> ${WORKSPACE}/src/image_based_formation_control/config/params.yaml
+mkdir -p ${WORKSPACE}/image_based_formation_control/output/0
+mkdir -p ${WORKSPACE}/image_based_formation_control/output/1
+mkdir -p ${WORKSPACE}/image_based_formation_control/output/2
+mkdir -p ${WORKSPACE}/image_based_formation_control/output/3
 fi
 
 if [ "$2" == "IBVS" ] || [ "$2" == "All" ]
 then
 cp -r Packages/vc_controller ${WORKSPACE}/src/
-echo "target_compile_definitions(vc_controller PUBLIC WORKSPACE=\"${WORKSPACE}\")" >> ${WORKSPACE}/src/vc_controller/CMakeLists.txt
+echo "target_compile_definitions(vc_controller PUBLIC WORKSPACE=\"${WORKSPACE}/\")" >> ${WORKSPACE}/src/vc_controller/CMakeLists.txt
+fi
+
+if [ "$2" == "Files" ] || [ "$2" == "All" ]
+then
+cp Files/launch/* ${WORKSPACE}/src/rotors_simulator/rotors_gazebo/launch/
+cp -r Files/models/* ~/.gazebo/models/
+cp -r Files/config/* ${WORKSPACE}/src/image_based_formation_control/config/
+cp Files/worlds/* ${WORKSPACE}/src/rotors_simulator/rotors_gazebo/worlds/
+cp Files/hummingbird.xacro ${WORKSPACE}/src/rotors_simulator/rotors_description/urdf/hummingbird.xacro
 fi
 
 if [ "$2" == "All" ]
@@ -39,4 +49,4 @@ then
 cp -r Packages/Placing ${WORKSPACE}/src/
 fi
 
-source ${WORKSPACE}/devel/setup.bash
+# source ${WORKSPACE}/devel/setup.bash
